9af248ec93067dab517d8f69d597b62082ab5212
Josep Pascual Badia <joseppascualbadia@gmail.com>
2026-06-28 17:25:01 +0200

    fixup: reformat with new printWidth, relax engines to >=24

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -5,8 +5,6 @@ import App from '../src/App'
 describe('App', () => {
   it('renders the hello world heading', () => {
     render(<App />)
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
-      'Hello, World!',
-    )
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hello, World!')
   })
 })
