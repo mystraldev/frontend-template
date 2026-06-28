27e910522a52daba718f241b8b4e14c561e80b8f
Josep Pascual Badia <joseppascualbadia@gmail.com>
2026-06-28 15:46:52 +0200

    chore: add coverage with 80% threshold

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,16 @@ export default defineConfig({
     setupFiles: './test/setup.ts',
     css: true,
     exclude: ['e2e/**', 'node_modules/**'],
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/main.tsx'],
+    },
   },
 })
